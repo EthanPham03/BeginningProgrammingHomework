7606287df67fe77a8ed1d1b7aef64a2f5fbe70e6
Ethan Pham <158526468+EthanPham03@users.noreply.github.com>
2024-08-23 13:39:36 -0500

    Update PC2-10.cpp

--- PC2-10.cpp
+++ PC2-10.cpp
@@ -1,3 +1,18 @@
 // Ethan Pham
-//
+// A C++ program to calculate miles per gallon
 #include <iostream>
+
+int main() {
+  double miles, gallons, mpg;
+  std::cout << "Enter miles traveled: ";
+  std::cin >> miles; 
+  std::cout << "Enter gallons of gas: ";
+  std::cin >> gallons;
+
+  if (gallons !=0) {
+    mpg = miles / gallons;
+    std::cout << "Miles per gallon: " << mpg << std:endl;
+  }
+
+  return 0;
+}
