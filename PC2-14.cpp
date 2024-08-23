653d60c7e3cbef20e019048a8298cbf10ba7a6b6
Ethan Pham <158526468+EthanPham03@users.noreply.github.com>
2024-08-23 15:34:48 -0500

    Update PC2-14.cpp

--- PC2-14.cpp
+++ PC2-14.cpp
@@ -1,1 +1,12 @@
+// Ethan Pham
+// A C++ program that displays personal information
 
+#include <iostream>
+
+int main() {
+  std::cout << "Ethan Pham" << std::endl;
+  std::cout << "1234 ABC street, OKC, OK, 73159" << std::endl;
+  std::cout << "8675309" << std::endl;
+  std::cout << "Computer Science" << std::endl;
+  return 0;
+}
