7b1bdf5db8f78e92bcef0f3deff670af24edba69
Ethan Pham <158526468+EthanPham03@users.noreply.github.com>
2024-09-30 17:46:47 -0500

    Update PC8-11.cpp

--- PC8-11.cpp
+++ PC8-11.cpp
@@ -1,4 +1,12 @@
 // A program that reads a file and sorts the strings
 #include <iostream>
+#include <string>
 using namespace std;
 
+int main()
+{
+  const int NUM_NAMES = 20;
+  string names[NUM_NAMES] = {"Collins, Bill", "Smith, Bart", "Allen, Jim", "Griffin, Jim", "Stamey, Marty", "Rose, Geri", "Taylor, Terri", "Johnson, Jill", "Allison, Jeff", "Looney, Joe", "Wolfe, Bill", "James, Jean", "Weaver, Jim", "Pore, Bob", "Rutherford, Greg", "Javens, Renee", "Harrison, Rose", "Setzer, Cathy", "Pike, Gordon", "Holland, Beth"};
+
+return 0;
+}
