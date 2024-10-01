4f8d3456d89f59efe2fd0aa78b4d83e1d4e03ba4
Ethan Pham <158526468+EthanPham03@users.noreply.github.com>
2024-10-01 15:50:38 -0500

    Update PC8-11.cpp

--- PC8-11.cpp
+++ PC8-11.cpp
@@ -1,12 +1,46 @@
 // A program that reads a file and sorts the strings
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <algorithm> 
 using namespace std;
 
 int main()
 {
-  const int NUM_NAMES = 20;
-  string names[NUM_NAMES] = {"Collins, Bill", "Smith, Bart", "Allen, Jim", "Griffin, Jim", "Stamey, Marty", "Rose, Geri", "Taylor, Terri", "Johnson, Jill", "Allison, Jeff", "Looney, Joe", "Wolfe, Bill", "James, Jean", "Weaver, Jim", "Pore, Bob", "Rutherford, Greg", "Javens, Renee", "Harrison, Rose", "Setzer, Cathy", "Pike, Gordon", "Holland, Beth"};
+    const int NUM_NAMES = 50;
+    string names[NUM_NAMES];
+    string fileName;
 
-return 0;
+    cout << "Enter the file name: ";
+    cin >> fileName;
+
+    ifstream inputFile(fileName);
+    if (!inputFile)
+    {
+        cout << "Error opening file " << fileName << endl;
+        return 1;
+    }
+
+    for (int i = 0; i < NUM_NAMES; i++)
+    {
+        getline(inputFile, names[i]);
+    }
+    
+    inputFile.close(); 
+
+    cout << "\nUnsorted Names:\n";
+    for (int i = 0; i < NUM_NAMES; i++)
+    {
+        cout << names[i] << endl;
+    }
+
+    sort(names, names + NUM_NAMES);
+
+    cout << "\nSorted Names:\n";
+    for (int i = 0; i < NUM_NAMES; i++)
+    {
+        cout << names[i] << endl;
+    }
+
+    return 0;
 }
